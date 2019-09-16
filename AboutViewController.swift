41e0d38867d290224b83147f26571c1d3b00c6e5
Bryle Tayag <techtayag@gmail.com>
2019-09-15 22:36:58 -0700

    BullsEye1.xcodeproj

---
+++ AboutViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AboutViewController.swift
+//  BullsEye1
+//
+//  Created by Brown Lotus on 6/14/19.
+//  Copyright © 2019 techtayag. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+        }
+    
+    @IBAction func author(){
+        dismiss(animated: true, completion: nil)
+        }
+    
+    }
---
+++ BullsEye1/AuthorViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AuthorViewController.swift
+//  BullsEye1
+//
+//  Created by Brown Lotus on 6/14/19.
+//  Copyright © 2019 techtayag. All rights reserved.
+//
+
+import UIKit
+
+class AuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+ 
+    @IBAction func Author() {
+        dismiss(animated: true, completion: nil)
+    
+    }
+
+}
